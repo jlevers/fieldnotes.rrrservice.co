61d1f479ef0bd07ed58fd1ce47a97dc39b3e60b6
jesse <jesse@jesseevers.com>
2026-02-14 16:19:32 -0800

    Highlight search results

--- src/js/search.js
+++ src/js/search.js
@@ -13,11 +13,47 @@
     var bodyEl = card.querySelector(".note-card-body");
     return {
       el: card,
+      bodyEl: bodyEl,
+      originalHTML: bodyEl ? bodyEl.innerHTML : "",
       project: card.dataset.project || "",
       tags: (card.dataset.tags || "").split(",").filter(Boolean),
       body: bodyEl ? bodyEl.textContent.toLowerCase() : "",
     };
   });
+
+  function escapeRegExp(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  }
+
+  function highlightText(node, regex) {
+    if (node.nodeType === 3) {
+      var match = node.nodeValue.match(regex);
+      if (match) {
+        var span = document.createElement("span");
+        var before = node.nodeValue.substring(0, match.index);
+        var after = node.nodeValue.substring(match.index + match[0].length);
+        span.innerHTML =
+          escapeHTML(before) +
+          "<mark>" + escapeHTML(match[0]) + "</mark>" +
+          escapeHTML(after);
+        node.parentNode.replaceChild(span, node);
+        // Continue highlighting in the remaining text
+        var lastChild = span.lastChild;
+        if (lastChild && lastChild.nodeType === 3) {
+          highlightText(lastChild, regex);
+        }
+      }
+    } else if (node.nodeType === 1 && node.nodeName !== "MARK") {
+      var children = Array.from(node.childNodes);
+      for (var i = 0; i < children.length; i++) {
+        highlightText(children[i], regex);
+      }
+    }
+  }
+
+  function escapeHTML(str) {
+    return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  }
 
   function applyFilters() {
     var query = (searchInput ? searchInput.value : "").toLowerCase().trim();
@@ -39,6 +75,15 @@
       if (query && show) {
         var haystack = d.project + " " + d.tags.join(" ") + " " + d.body;
         if (haystack.indexOf(query) === -1) show = false;
+      }
+
+      // Restore original HTML first, then highlight if needed
+      if (d.bodyEl) {
+        d.bodyEl.innerHTML = d.originalHTML;
+        if (query && show) {
+          var regex = new RegExp(escapeRegExp(query), "i");
+          highlightText(d.bodyEl, regex);
+        }
       }
 
       d.el.style.display = show ? "" : "none";
